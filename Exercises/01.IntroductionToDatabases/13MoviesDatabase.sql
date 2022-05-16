6b08e196d9d11f0e677b2124453f4ebcc12e05fa
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-17 00:42:59 +0300

    Update 13MoviesDatabase.sql

--- Exercises/01.IntroductionToDatabases/13MoviesDatabase.sql
+++ Exercises/01.IntroductionToDatabases/13MoviesDatabase.sql
@@ -58,9 +58,15 @@ VALUES
 INSERT INTO Categories (Id,CategoryName,Notes)
 VALUES
 (1, 'Horor', 'Film that seeks to elicit fear or disgust in its audience for entertainment purposes'),
-(2, 'Fiction', 'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations'),
-(3, 'Art', 'An art film (or art house film) is typically an independent film, aimed at a niche market rather than a mass market audience.[1] It is "intended to be a serious, artistic work, often experimental and not designed for mass appeal",[2] "made primarily for aesthetic reasons rather than commercial profit",[3] and contains "unconventional or highly symbolic content"'),
-(4, 'Musical', 'Musical film is a film genre in which songs by the characters are interwoven into the narrative, sometimes accompanied by singing and dancing. The songs usually advance the plot or develop the films characters, but in some cases, they serve merely as breaks in the storyline, often as elaborate "production numbers".'),
+(2, 'Fiction', 'Science fiction (sometimes shortened to sci-fi or SF) is a genre of speculative fiction which typically deals with imaginative and futuristic 
+ concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the 
+ "literature of ideas", and it often explores the potential consequences of scientific, social, and technological innovations'),
+(3, 'Art', 'An art film (or art house film) is typically an independent film, aimed at a niche market rather than a mass market audience.[1] It is "intended 
+ to be a serious, artistic work, often experimental and not designed for mass appeal",[2] "made primarily for aesthetic reasons rather than commercial profit",
+ [3] and contains "unconventional or highly symbolic content"'),
+(4, 'Musical', 'Musical film is a film genre in which songs by the characters are interwoven into the narrative, sometimes accompanied by singing and dancing.
+ The songs usually advance the plot or develop the films characters, but in some cases, they serve merely as breaks in the storyline, often as elaborate
+ "production numbers".'),
 (5, 'Amateur', 'Amateur film is the low-budget hobbyist art of film practised for passion and enjoyment and not for business purposes.');
 
 ALTER TABLE Movies ALTER COLUMN Rating DECIMAL(3,1);
@@ -77,6 +83,7 @@ Batman must accept one of the greatest psychological and physical tests of his a
 (4,'The Lord of the Rings', 4, 2003, '03:21:36', 4, 2, 9.0,
 'Gandalf and Aragorn lead the World of Men against Sauron`s army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.'),
 (5, 'Schindlers List', 5, 1993, '03:11:56', 1, 1, 9.0,
-'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.');
+'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their
+ persecution by the Nazis.');
 
 SELECT * FROM Movies;
