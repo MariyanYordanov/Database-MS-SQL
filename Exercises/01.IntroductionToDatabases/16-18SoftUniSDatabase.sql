159d3647f21ec42119c6bfdc7eac3459e043915a
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-17 17:24:39 +0300

    Add 16-18 tasks

--- Exercises/01.IntroductionToDatabases/16-18SoftUniSDatabase.sql
+++ Exercises/01.IntroductionToDatabases/16-18SoftUniSDatabase.sql
@@ -38,3 +38,32 @@ CREATE TABLE Employees
 USE SoftUni
 BACKUP DATABASE SoftUni
 TO DISK = 'H:\backups\softuni-backup.bak';
+
+-- 18 Basic Insert
+USE SoftUni
+INSERT INTO Towns
+VALUES
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas');
+
+SELECT * FROM Towns;
+
+INSERT INTO Departments
+VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance');
+
+SELECT * FROM Departments;
+
+INSERT INTO Employees(FirstName,MiddleName,LastName,JobTitle,DepartmentId,HireDate,Salary)
+VALUES
+('Ivan','Ivanov','Ivanov','.NET Developer',4,'02/01/2013',3500.00),
+('Petar','Petrov','Petrov','Senior Engineer',1,'03/02/2004',4000.00),
+('Maria','Petrova','Ivanova','Intern',5,'08/28/2016',525.25),
+('Georgi','Teziev','Ivanov','CEO',2,'12/09/2007',3000.00),
+('Peter','Pan','Pan','Intern',3,'08/28/2016',599.88);
