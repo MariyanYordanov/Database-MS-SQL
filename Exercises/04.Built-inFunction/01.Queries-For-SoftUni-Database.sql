91d91daaa6d2946b7e4f212111b4af5329845f82
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-27 00:42:17 +0300

    Add query

--- Exercises/04.Built-inFunction/01.Queries-For-SoftUni-Database.sql
+++ Exercises/04.Built-inFunction/01.Queries-For-SoftUni-Database.sql
@@ -53,3 +53,20 @@ WHERE [HireDate] >= '2001';
 SELECT [FirstName],[LastName]
 FROM [Employees]
 WHERE LEN([LastName]) = 5;
+
+-- 10. Rank Employees by Salary
+SELECT [EmployeeID],[FirstName],[LastName], [Salary],
+DENSE_RANK() OVER (PARTITION BY [Salary] ORDER BY [EmployeeID]) AS [Rank]
+FROM [Employees]
+WHERE [Salary] BETWEEN 10000 AND 50000
+ORDER BY [Salary] DESC;
+
+-- 11. Find All Employees with Rank 2 
+
+SELECT *
+FROM (SELECT [EmployeeID],[FirstName],[LastName],[Salary],
+       DENSE_RANK() over (partition by Salary ORDER BY EmployeeID) AS Rank
+       FROM Employees
+       WHERE Salary BETWEEN 10000 AND 50000) AS [Rank2]
+WHERE Rank = 2
+ORDER BY Salary DESC;
