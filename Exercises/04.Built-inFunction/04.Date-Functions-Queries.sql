3546a83bfa6073aedfeba846dc7c1b9c22031024
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-28 02:05:12 +0300

    Add queries

---
+++ Exercises/04.Built-inFunction/04.Date-Functions-Queries.sql
@@ -0,0 +1,29 @@
+-- 18. Orders Table
+USE Orders;
+SELECT [ProductName],[OrderDate],
+DATEADD(DAY,3,[OrderDate]) AS [Pay Due],
+DATEADD(MONTH,1,[OrderDate]) AS [Deliver Due]
+FROM [Orders];
+
+-- 19. People Table
+USE Orders;
+CREATE TABLE [People]
+(
+	[Id] INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(100) NOT NULL,
+	[Birthdate] DATE NOT NULL
+);
+
+INSERT INTO [People] 
+VALUES
+('Victor','2000-12-07'),
+('Steven','1992-09-10'),
+('Stephen','1910-09-19'),
+('John','2010-01-06');
+
+SELECT [Name],
+DATEDIFF(YEAR,[Birthdate],GETDATE()) AS [Age in Years],
+DATEDIFF(MONTH,[Birthdate],GETDATE()) AS [Age in Months],
+DATEDIFF(DAY,[Birthdate],GETDATE()) AS [Age in Days],
+DATEDIFF(MINUTE,[Birthdate],GETDATE()) AS [Age in Minutes]
+FROM [People];
