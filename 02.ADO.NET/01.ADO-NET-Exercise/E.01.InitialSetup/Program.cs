98e8a99920be5f2aab82a59cf2b7958d694370cd
Mariyan Yordanov <marsrewq@gmail.com>
2022-06-30 09:46:43 +0300

    Add new method in ADO.NET exercise

--- 02.ADO.NET/01.ADO-NET-Exercise/E.01.InitialSetup/Program.cs
+++ 02.ADO.NET/01.ADO-NET-Exercise/E.01.InitialSetup/Program.cs
@@ -8,14 +8,19 @@ namespace E._01.InitialSetup
     {
         static void Main(string[] args)
         {
-            
+            int villainsId = int.Parse(Console.ReadLine());
+
             using SqlConnection sqlConnection = new SqlConnection(Config.ConnectionString);
+
             sqlConnection.Open();
-            string result = GetVillainsWithMinionsCount(sqlConnection);
+            string result = GetAllMinionsNamePerVillain(sqlConnection, villainsId);
             Console.WriteLine(result);
+
             sqlConnection.Close();
-            
         }
+
+
+        //2.Villain Names
 
         /// <summary>
         /// Take an open sql connection, connects to the database and returns all villains with muinions
@@ -37,6 +42,7 @@ namespace E._01.InitialSetup
                               GROUP BY [v].[Name]
                           HAVING COUNT([mv].[VillainId]) > 3 
                         ORDER BY COUNT([mv].[VillainId])";
+
             SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnection);
             using SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
             while (sqlDataReader.Read())
@@ -47,7 +53,71 @@ namespace E._01.InitialSetup
             sqlDataReader.Close();
 
             return output.ToString().TrimEnd();   
-        } 
+        }
 
+
+        // 3.Minion Names
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sqlConnection"></param>
+        /// <param name="villainId"></param>
+        /// <returns></returns>
+        private static string GetAllMinionsNamePerVillain(SqlConnection sqlConnection, int villainId)
+        {
+            StringBuilder output = new StringBuilder();
+
+            string sqlQuery = @" SELECT [Name] 
+                                   FROM [Villains] 
+                                  WHERE [Id] = @VillainId";
+
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnection);
+
+            // To avoid SQL indjection
+            sqlCommand.Parameters.AddWithValue("@VillainId", villainId);
+
+            string villainName = (string)sqlCommand.ExecuteScalar();
+
+            if (villainName == null)
+            {
+                return $"No villain with ID {villainId} exists in the database.";
+            }
+
+            output.AppendLine($"Villain: {villainName}");
+
+            string minionsQuery = @"SELECT [m].[Name],
+                                           [m].[Age]
+                                      FROM [MinionsVillains]
+                                        AS [mv]
+                                 LEFT JOIN [Minions]
+                                        AS [m]
+                                        ON [m].[Id] = [mv].[MinionId]
+                                     WHERE [mv].[VillainId] = @VillainId
+                                  ORDER BY [m].[Name]";
+
+            SqlCommand sqlCmdGetMinions = new SqlCommand(minionsQuery, sqlConnection);
+
+            // To avoid SQL indjection
+            sqlCmdGetMinions.Parameters.AddWithValue("@VillainId", villainId);
+
+            using SqlDataReader minionsReader = sqlCmdGetMinions.ExecuteReader();
+            if (!minionsReader.HasRows)
+            {
+                output.AppendLine($"(no minions)");
+            }
+            else
+            {
+                int rowNumber = 1;
+                while (minionsReader.Read())
+                {
+                    output.AppendLine($"{rowNumber}. {minionsReader["Name"]} {minionsReader["Age"]}");
+                    rowNumber++;
+                }
+            }
+            
+            minionsReader.Close();
+
+            return output.ToString().TrimEnd();
+        }
     }
 }
