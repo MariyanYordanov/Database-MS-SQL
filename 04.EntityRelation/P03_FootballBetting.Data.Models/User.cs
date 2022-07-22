5b7eae4a35cff7d90c55353d5ee111fd966486f4
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-22 21:16:12 +0300

    Add new task

---
+++ 04.EntityRelation/P03_FootballBetting.Data.Models/User.cs
@@ -0,0 +1,39 @@
+﻿namespace P03_FootballBetting.Data.Models
+{
+    using P03_FootballBetting.Data.Common;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class User
+    {
+        public User()
+        {
+            this.Bets = new HashSet<Bet>();
+        }
+
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConastants.UsernameMaxLength)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConastants.UserPasswordMaxLength)]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConastants.UserEmailMaxLength)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConastants.UserNameMaxLength)]
+        public string Name { get; set; }
+
+        public decimal Balance { get; set; }
+
+        [InverseProperty(nameof(Bet.User))]
+        public virtual ICollection<Bet> Bets { get; set; }
+    }
+}
