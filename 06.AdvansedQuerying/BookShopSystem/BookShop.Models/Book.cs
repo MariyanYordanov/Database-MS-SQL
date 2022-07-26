d34aee40df5441907b51a36d86b14526b44bc639
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-26 22:30:55 +0300

    Add new task

---
+++ 06.AdvansedQuerying/BookShopSystem/BookShop.Models/Book.cs
@@ -0,0 +1,40 @@
+﻿using BookShop.Models.Enums;
+
+namespace BookShop.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Book
+    {
+        public Book()
+        {
+            this.BookCategories = new HashSet<BookCategory>();
+        }
+
+        [Key]
+        public int BookId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual EditionType EditionType { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Copies { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        public virtual AgeRestriction AgeRestriction { get; set; }
+
+        [ForeignKey(nameof(Author))]
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+
+        public virtual ICollection<BookCategory> BookCategories { get; set; }
+    }
+}
---
+++ 06.AdvansedQuerying/BookShopSystem/BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop
+{
+    using Data;
+    using System;
+    using Initializer;
+    using System.Linq;
+    using System.Text;
+    using BookShop.Models.Enums;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string command = Console.ReadLine();
+            
+            Console.WriteLine(GetBooksByAgeRestriction(context, command));
+        }
+
+        // 2.Age Restriction
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            bool parsed = Enum.TryParse(command, true, out AgeRestriction age);
+
+            if (!parsed)
+            {
+                return String.Empty;
+            }
+
+            string[] bookTitles = context.Books
+                .Where(b => b.AgeRestriction == age)
+                .Select(b => b.Title)
+                .OrderBy(t => t)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, bookTitles);
+        }
+    }
+}
