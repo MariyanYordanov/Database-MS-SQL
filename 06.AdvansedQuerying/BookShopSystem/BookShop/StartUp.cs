0e3b2cda46863712a91739d94f0e798b2ef81c88
Mariyan Yordanov <marsrewq@gmail.com>
2022-07-27 23:27:41 +0300

    Add new task

--- 06.AdvansedQuerying/BookShopSystem/BookShop/StartUp.cs
+++ 06.AdvansedQuerying/BookShopSystem/BookShop/StartUp.cs
@@ -296,5 +296,22 @@
 
             context.SaveChanges();
         }
+
+        // 16.Remove Books
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(x => x.Copies < 4200)
+                .ToList();
+
+            foreach (var book in books)
+            {
+                context.Books.Remove(book);
+            }
+
+            context.SaveChanges();
+
+            return books.Count;
+        }
     }
 }
