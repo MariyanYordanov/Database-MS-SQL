bce54cbaf338c1f2267ab59d091f5cc67de8b96c
Mariyan Yordanov <marsrewq@gmail.com>
2022-08-01 12:44:19 +0300

    Add new task

--- 09.XML/09. XML-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
+++ 09.XML/09. XML-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
@@ -1,25 +1,30 @@
 ﻿namespace ProductShop
 {
-    using System;
     using System.IO;
     using System.Linq;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
     using CarDealer.XmlHelper;
     using ProductShop.Data;
+    using ProductShop.Dtos.Export;
     using ProductShop.Dtos.Import;
     using ProductShop.Models;
 
     public class StartUp
     {
+        static IMapper mapper;
+
+        
         public static void Main(string[] args)
         {
             ProductShopContext dbContext = new ProductShopContext();
-
             //dbContext.Database.EnsureDeleted();
             //dbContext.Database.EnsureCreated();
 
-            string inputXml = File.ReadAllText("../../../Datasets/products.xml");
-            string result = ImportProducts(dbContext, inputXml);
-            Console.WriteLine(result);
+            //string inputXml = File.ReadAllText("../../../Datasets/categories-products.xml");
+
+            string result = GetProductsInRange(dbContext);
+            File.WriteAllText("../../../Outputs/products-in-range.xml", result);
         }
 
         // Query 1. Import Users
@@ -62,5 +67,59 @@
 
             return $"Successfully imported {products.Length}";
         }
+
+        // Query 3. Import Categories
+        public static string ImportCategories(ProductShopContext context, string inputXml)
+        {
+            var categoriesDto = XmlConverter.Deserializer<ImportCategoryDto>(inputXml,"Categories");
+
+            var categories = categoriesDto.Select(c => new Category { Name = c.Name }).ToArray();
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Length}";
+        }
+
+        // Query 4. Import Categories and Products
+        public static string ImportCategoryProducts(ProductShopContext context, string inputXml)
+        {
+            var categoriesProducts = XmlConverter.Deserializer<ImportCategoriesProductsDto>(inputXml, "CategoryProducts");
+
+            var categoryProducts = categoriesProducts
+                .Where(x => context.Products
+                    .Any(y => y.Id == x.ProductId) && context.Categories.
+                        Any(y => y.Id == x.CategoryId))
+                .Select(cp => new CategoryProduct
+                {
+                    CategoryId = cp.CategoryId,
+                    ProductId = cp.ProductId,
+                })
+                .ToArray();
+
+            context.CategoryProducts.AddRange(categoryProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoryProducts.Length}";
+        }
+
+        // Query 5. Export Products In Range
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            ExportProductsInRangeDto[] productsInRange = context.Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .Select(p => new ExportProductsInRangeDto
+                {
+                    Name = p.Name,
+                    Price = p.Price,
+                    FullName = $"{p.Buyer.FirstName} {p.Buyer.LastName}" 
+                })
+                .OrderBy(p => p.Price)
+                .Take(10)
+                .ToArray();
+
+            return XmlConverter.Serialize<ExportProductsInRangeDto>(productsInRange, "Products");
+        }
+
     }
 }
