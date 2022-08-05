de0be25f08642d2ec6d284828e33d06dae37039a
Mariyan Yordanov <marsrewq@gmail.com>
2022-08-05 23:05:05 +0300

    Add Old Exam

---
+++ SoftJail/01. Model Definition_Skeleton and Datasets/SoftJail/DataProcessor/ImportDto/MailImportDto.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [JsonObject]
+    public class MailImportDto
+    {
+        [Required]
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [JsonProperty("Sender")]
+        public string Sender { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        [RegularExpression(@"^[\d\w\s]*\sstr.$")]
+        [JsonProperty("Address")]
+        public string Address { get; set; }
+    }
+}
